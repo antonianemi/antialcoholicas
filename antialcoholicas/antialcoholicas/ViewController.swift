3f5dd9e31c8c14bc240d6d8ff1174d3d4a06fcf9
Antonio Cortes <antoni_anemi@hotmail.com>
2018-06-08 08:21:16 -0700

    adding FirebaseAuth, integration for login with email working success

--- antialcoholicas/antialcoholicas/ViewController.swift
+++ antialcoholicas/antialcoholicas/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbl_username:UITextField!
+    @IBOutlet weak var lbl_password:UITextField!
+    @IBOutlet weak var btn_logIn:UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func logInfunc(_ sender: Any?) {
+        guard let email = lbl_username.text, let password = lbl_password.text else {return}
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if error != nil{
+                //fail
+                print("fail")
+            }
+            else{
+                //success
+                print("success")
+            }
+        }
+    }
 }
-
